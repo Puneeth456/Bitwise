be7b7e09d6629c8958c36418b9388f8c90d7988d
puneeth456 <shivanpuneeth50@gmail.com>
2019-06-19 18:32:52 +0530

    input function

---
+++ Input.py
@@ -0,0 +1,12 @@
+# x=int(input("Enter the first number"))
+# y=int(input("Enter the second number"))
+# z=x+y
+# print(z)
+
+#
+# chr=input("Enter the character")[0]
+# print(chr[0])
+
+result=eval(input("enter the expression"))
+print(result)
+
